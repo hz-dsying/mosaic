c59130d75546778b15a23fc9fcf3b1b0f1e4f208
dsy <dsy@DESKTOP-FUGQT5S>
2020-04-14 00:49:26 +0800

    项目新建提交

---
+++ src/test/java/com/dsy/main/MosaicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsy.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosaicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
